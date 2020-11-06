3c3fd2ff41c0172997f55b4b059e55230c70e47f
Diego Eiras <eiras.lucio@gmail.com>
2020-11-06 19:22:44 -0300

    :tada: Create a new Vue project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style>
+  #app {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
