4d6d1eba488e34e68a99263416c29c92d79537d7
Diego Eiras <eiras.lucio@gmail.com>
2020-11-07 00:41:06 -0300

    📦  ADD: Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer>
+    <img alt="Vue" src="../assets/logo.png">
+    <p>
+      Criado com Vue.js por 
+      <a href="https://www.github.com/lucioeiras" target="_blank">
+        Diego Eiras.
+      </a>
+    </p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer',
+  }
+</script>
+
+<style>
+  footer {
+    display: flex;
+    align-items: center;
+
+    margin-top: 80px;
+  }
+
+  footer img {
+    width: 32px;
+    height: 32px;
+
+    margin-right: 8px;
+  }
+
+  footer p {
+    color: var(--text);
+    font-size: 20px;
+    font-weight: 300;
+  }
+
+  footer p a {
+    color: var(--text);
+  }
+</style>
