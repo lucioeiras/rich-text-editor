e6e4b4eea0a553a73553a60bcb3f2b49d9201abb
Diego Eiras <eiras.lucio@gmail.com>
2020-11-09 10:17:37 -0300

    ✒️  EDIT: Change to div with contenteditable

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -4,37 +4,14 @@
       <button 
         v-bind:key="index" 
         v-for="(action, index) in actions"
-        :class="[
-          action.class,
-          action.isActive && 'active'
-        ]"
+        :class="action.class"
         @click="action.onClick"
       >{{ action.display }}</button>
     </div>
 
     <hr>
 
-    <div id="text-inputs">
-      <input
-        v-bind:key="index"  
-        v-for="(line, index) in lines"
-        v-model="line.value"
-
-        @keyup.enter="goToNextLine(index)"
-        @keyup.delete="removeLine(index)"
-        @keyup.up="moveUp(index)"
-        @keyup.down="moveDown(index)"
-        @focus="getFocus(index)"
-
-        :ref="String(index)"
-        :class="[
-          line.isTitle && 'title',
-          line.isBold && 'bold',
-          line.isItalic && 'italic',
-          line.isUnderlined && 'underlined'
-        ]"
-      />
-    </div>
+    <div ref="editor" class="text-editor" contenteditable="true"></div>
   </div>
 </template>
 
@@ -48,153 +25,35 @@
           {
             display: 'T',
             class: 'title',
-            isActive: false,
-            onClick: () => this.toogleAction('isTitle'),
+            onClick: () => this.toogleAction('formatblock', 'h1'),
           },
           {
             display: 'B',
             class: 'bold',
-            isActive: false,
-            onClick: () => this.toogleAction('isBold'),
+            onClick: () => this.toogleAction('bold', null),
           },
           {
             display: 'I',
             class: 'italic',
-            isActive: false,
-            onClick: () => this.toogleAction('isItalic'),
+            onClick: () => this.toogleAction('italic', null),
           },
           {
             display: 'U',
             class: 'underlined',
-            isActive: false,
-            onClick: () => this.toogleAction('isUnderlined'),
+            onClick: () => this.toogleAction('underline', null),
           },
         ],
-        lines: []
       }
     },
 
     methods: {
-      addLine() {
-        this.lines.push({ 
-          value: '',
-          focus: false,
-
-          isTitle: false,
-          isBold: false,
-          isItalic: false,
-          isUnderlined: false,
-        });
-      },
-
-      goToNextLine(lineIndex) {
-        if (lineIndex === this.lines.length - 1) {
-          this.addLine();
-        }
-
-        const nextLine = String(lineIndex + 1);
-        this.$refs[nextLine].focus();
-      },
-
-      removeLine(lineIndex) {
-        if (
-          this.lines[lineIndex].value === '' 
-          && this.lines.length > 1
-        ) {
-          this.lines.splice(lineIndex, 1);
-
-          const previousLine = String(lineIndex - 1);
-          this.$refs[previousLine].focus();
-        }
-      },
-
-      moveUp(lineIndex) {
-        if (lineIndex === 0) {
-          return
-        }
-
-        const previousLine = String(lineIndex - 1);
-        this.$refs[previousLine].focus();
-      },
-
-      moveDown(lineIndex) {
-        if (lineIndex === this.lines.length - 1) {
-          return
-        }
-
-        const nextLine = String(lineIndex + 1);
-        this.$refs[nextLine].focus();
-      },
-
-      getFocus(lineIndex) {
-        this.lines.map((line, index) => {
-          this.actions.map(action => {
-            if (action.isActive) {
-              action.isActive = false;
-            }
-
-            return action;
-          });
-
-          if (line.focus) {
-            line.focus = false;
-          }
-
-          if (lineIndex === index) {
-            line.focus = true;
-
-            this.lines.forEach(line => {
-              if (line.isTitle) {
-                this.actions[0].isActive = true;
-              }
-
-              if (line.isBold) {
-                this.actions[1].isActive = true;
-              }
-
-              if (line.isItalic) {
-                this.actions[2].isActive = true;
-              }
-
-              if (line.isUnderlined) {
-                this.actions[3].isActive = true;
-              }
-            });
-          }
-
-          return line;
-        });
-      },
-
-      toogleAction(action) {
-        this.lines.map(line => {
-          if (line.focus) {
-            line[action] = !line[action];
-          }
-
-          if (action === 'isTitle') {
-            this.actions[0].isActive = true;
-          }
-
-          if (action === 'isBold') {
-            this.actions[1].isActive = true;
-          }
-
-          if (action === 'isItalic') {
-            this.actions[2].isActive = true;
-          }
-
-          if (action === 'isUnderlined') {
-            this.actions[3].isActive = true;
-          }
-
-          return line;
-        });
+      toogleAction(action, value) {
+        document.execCommand(action, false, value);
       }
     },
 
     mounted() {
-      for (let i = 0; i < 10; i++) this.addLine();
+      this.$refs.editor.focus();
     },
   }
 </script>
@@ -243,40 +102,24 @@
     .active {
       color: $primary;
     }
-  }
 
-  #text-inputs {
-    display: flex;
-    flex-direction: column;
-
-    input {
-      max-width: 920;
-
-      border: none;
-      background: none;
-
-      color: $text;
-      font-size: 20px;
-
-      padding: 4px 0;
+    .bold {
+      font-weight: 700;
     }
 
-    .title {
-      font-size: 48px;
-      font-weight: 900;
-      padding-bottom: 8px;
+    .italic {
+      font-style: italic;
+    }
+
+    .underlined {
+      text-decoration-line: underline;
     }
   }
 
-  .bold {
-    font-weight: 700;
-  }
+  .text-editor {
+    height: 320px;
 
-  .italic {
-    font-style: italic;
-  }
-
-  .underlined {
-    text-decoration-line: underline;
+    color: $text;
+    font-size: 20px;
   }
 </style>
